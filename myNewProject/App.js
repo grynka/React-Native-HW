2923d334763eff9bde41b63651ed7712a3aac539
grynka <ihor.kozhemyakin@gmail.com>
2023-01-05 20:46:24 +0200

    Update App.js

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,15 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { TextInput, StyleSheet, Text, View, Button } from 'react-native';
 import { useState } from 'react';
+import { Header, Icon } from "react-native-elements";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 
 export default function App() {
   const [value, setValue] = useState("");
   const inputHandler = (text) => setValue(text);
   return (
     <>
+    <Header
+      backgroundImageStyle={{}}
+      barStyle="default"
+      centerComponent={{
+        text: "AVTOPARTS EXPERT",
+        style: { color: "#fff" }
+      }}
+      centerContainerStyle={{}}
+      containerStyle={{ width: 350 }}
+      leftComponent={{ icon: "menu", color: "#fff" }}
+      leftContainerStyle={{}}
+      linearGradientProps={{}}
+      placement="center"
+      rightComponent={{ icon: "home", color: "#fff" }}
+      rightContainerStyle={{}}
+      statusBarProps={{}}
+    />
 <View style={styles.container}>
-      <TextInput
-        placeholder="Type text"
+      <TextInput style={styles.input}
+        placeholder="Type Username"
         value={value}
         onChangeText={inputHandler}
       />
@@ -26,4 +46,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
+  input: {
+    border: '1px solid black',
+    height: 20,
+    fontSize: 16,
+  },
 });

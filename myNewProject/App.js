88e8fc25988fd5c6f5d9d907215dc66c8ebb1c22
grynka <ihor.kozhemyakin@gmail.com>
2023-01-05 23:10:07 +0200

    Update App.js

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,54 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { TextInput, StyleSheet, Text, View, Button } from 'react-native';
-import { useState } from 'react';
-import { Header, Icon } from "react-native-elements";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform
+} from "react-native";
 
 export default function App() {
   const [value, setValue] = useState("");
   const inputHandler = (text) => setValue(text);
   return (
-    <>
-    <Header
-      backgroundImageStyle={{}}
-      barStyle="default"
-      centerComponent={{
-        text: "AVTOPARTS EXPERT",
-        style: { color: "#fff" }
-      }}
-      centerContainerStyle={{}}
-      containerStyle={{ width: 350 }}
-      leftComponent={{ icon: "menu", color: "#fff" }}
-      leftContainerStyle={{}}
-      linearGradientProps={{}}
-      placement="center"
-      rightComponent={{ icon: "home", color: "#fff" }}
-      rightContainerStyle={{}}
-      statusBarProps={{}}
-    />
-<View style={styles.container}>
-      <TextInput style={styles.input}
-        placeholder="Type Username"
-        value={value}
-        onChangeText={inputHandler}
-      />
-    </View>
-    <Button title="Log in">Log in</Button></>
+      <View style={styles.container}>
+      <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"}>
+        <TextInput
+          placeholder="Type text"
+          value={value}
+          onChangeText={inputHandler}
+        />
+              </KeyboardAvoidingView>
+
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  input: {
-    border: '1px solid black',
-    height: 20,
-    fontSize: 16,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingBottom: 30
   },
 });

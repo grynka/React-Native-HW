ee0c3c481c0b3a95cf1368ab99e70be566bda541
grynka <ihor.kozhemyakin@gmail.com>
2023-01-08 12:10:20 +0200

    upd

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,6 +1,3 @@
-import React, { useState } from "react";
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
 import LoginScreen from "./Screens/LoginScreen";
 
 export default function App() {
--- myNewProject/Screens/LoginScreen.js
+++ myNewProject/Screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import * as Font from "expo-font";
+import  AppLoading from "expo";
 import {
   StyleSheet,
   View,
@@ -10,79 +10,126 @@ import {
   KeyboardAvoidingView,
   Platform,
   Alert,
-  Button,
   ImageBackground,
+  Text,
+  TouchableOpacity,
 } from "react-native";
 
+const loadFonts = async () => {
+  await Font.loadAsync({
+    "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("../assets/fonts/Roboto-Medium.ttf"),
+  });
+};
+
 export default function LoginScreen() {
-    const [name, setName] = useState("");
-    const [password, setPassword] = useState("");
-  
-    const nameHandler = (text) => setName(text);
-    const passwordHandler = (text) => setPassword(text);
-  
-    const onLogin = () => {
-      Alert.alert("Credentials", `${name} + ${password}`);
-    };
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [isReady, setIsReady] = useState(false);
 
-    
-return(
-    <ImageBackground source={require('../assets/BG.jpg')} resizeMode="stretch" style={styles.image}>
+  const nameHandler = (text) => setName(text);
+  const passwordHandler = (text) => setPassword(text);
+
+  const onLogin = () => {
+    Alert.alert("Credentials", `${name} + ${password}`);
+  };
+
+  if (!isReady) {
+    return <AppLoading startAsync={loadFonts} onFinish={() => setIsReady(true)}/>
+};
+
+  return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-
-        <KeyboardAvoidingView
-          behavior={Platform.OS == "ios" ? "padding" : "height"}
+    <View style={styles.container}>
+       <ImageBackground
+       style={styles.image}
+       source={require("../assets/images//BG.jpg")} >
+              <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"}
         >
-          <TextInput
+       <View style={styles.form}>
+       
+        <Text style={styles.title}>Войти</Text>
+       <TextInput
             value={name}
             onChangeText={nameHandler}
-            placeholder="Username"
+            placeholder="Адрес электронной почты"
             style={styles.input}
           />
           <TextInput
             value={password}
             onChangeText={passwordHandler}
-            placeholder="Password"
+            placeholder="Пароль"
             secureTextEntry={true}
             style={styles.input}
           />
-          <Button title={"Войти"} style={styles.button} onPress={onLogin} />
-        </KeyboardAvoidingView>
-      </View>
-    </TouchableWithoutFeedback>
-    </ImageBackground>
-)
+          <TouchableOpacity activeOpacity={0.8} style={styles.loginButton} onPress={onLogin}>
+          <Text style={styles.loginButtonText} >Войти</Text>
+          </TouchableOpacity>
 
+       </View>
+            </KeyboardAvoidingView>
+      </ImageBackground>
+</View>
+    </TouchableWithoutFeedback>
+
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-        },
-    
-    input: {
-      width: 200,
-      height: 44,
-      padding: 10,
-      borderWidth: 1,
-      borderColor: "black",
-      marginBottom: 10,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
 
-    image: {
-        flex: 1,
-        justifyContent: "center"
-      },
-      button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 100,
-        elevation: 3,
-        backgroundColor:"#FF6C00",
-      }
-  });
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+  },
+
+  form: {
+    backgroundColor: "#FFFFFF",
+    paddingTop: 32,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25
+  },
+
+  input: {
+    height: 50,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    marginBottom: 10,
+    borderRadius: 8,
+    backgroundColor: "#F6F6F6",
+    color: "#212121",
+    fontSize: 16,
+    marginBottom: 16,
+    marginHorizontal: 16,
+
+  },
+
+  loginButton: {
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    marginHorizontal: 16,
+    marginTop: 27,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+  },
+
+  loginButtonText: {
+  color: "#FFFFFF",
+  fontSize: 16,
+  fontWeight: "400"
+  },
+
+  title: {
+    fontSize: 30,
+    textAlign: "center",
+    paddingTop: 32,
+    paddingBottom: 32,
+    fontFamily:"Roboto-Regular",
+  },
+});

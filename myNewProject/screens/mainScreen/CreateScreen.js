158a216b90b5860714b5496ac0f86503f365ae0d
grynka <ihor.kozhemyakin@gmail.com>
2023-01-12 03:21:18 +0200

    Update CreateScreen.js

--- myNewProject/screens/mainScreen/CreateScreen.js
+++ myNewProject/screens/mainScreen/CreateScreen.js
@@ -1,28 +1,59 @@
 import React, { useState, useEffect } from "react";
-import { TouchableOpacity, Image, View, Text, StyleSheet, TextInput } from "react-native";
+import {
+  TouchableOpacity,
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+} from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { FontAwesome } from "@expo/vector-icons";
-import { Feather, EvilIcons } from '@expo/vector-icons';
-import * as Location from 'expo-location';
-
+import { Feather, EvilIcons } from "@expo/vector-icons";
+import * as Location from "expo-location";
 
 export default function CreateScreen() {
   const [image, setImage] = useState(null);
+  const [name, setName] = useState('');
+  const [location, setLocation] = useState('');
+
+
+ const startUserLocationUpdates = async () => { 
+if (Platform.OS !== "web") {
+  const { status } = await Location.requestForegroundPermissionsAsync();
+  
+  if (status !== "granted") {
+    Alert.alert(
+      "Insufficient permissions!",
+      "Sorry, we need location permissions to make this work!",
+      [{ text: "Okay" }]
+    );
+    return;
+  }
+}
+  }
+
+ 
+
+const nameHandler = (text) => setName(text);
 
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
+    startUserLocationUpdates()
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     });
+    const loc = await Location.getCurrentPositionAsync()
 
     console.log(result);
 
     if (!result.canceled) {
+      setLocation({ latitude: loc.coords.latitude, longitude: loc.coords.longitude})
       setImage(result.assets[0].uri);
     }
+    console.log(location)
   };
 
   return (
@@ -44,13 +75,36 @@ export default function CreateScreen() {
           />
         </TouchableOpacity>
       </View>
-      <Text style={styles.fotoAction}>{image ? "Редактировать фото" : "Загрузите фото"}</Text>
-<TextInput placeholder="Название..." style={styles.name} />
-<TextInput placeholder="Местность..." style={styles.location} />
-<TouchableOpacity onPress={() => setImage(null)}>
-<Feather name="trash-2" size={24} color="#BDBDBD" />
-        </TouchableOpacity>
-        
+      <Text style={styles.fotoAction}>
+        {image ? "Редактировать фото" : "Загрузите фото"}
+      </Text>
+      <TextInput placeholder="Название..." style={styles.name} value={name}
+                  onChangeText={nameHandler}/>
+      <View style={styles.locationSection}>
+        <EvilIcons name="location" style={styles.locationIcon} size={24} />
+        <TextInput placeholder="Местность..." style={styles.location} value={location} />
+      </View><Text>{location && location.coords}</Text>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={{
+          ...styles.createButton,
+          backgroundColor: image ? "#FF6C00" : "#F6F6F6",
+        }}
+        onPress={{}}
+      >
+        <Text
+          style={{
+            color: image ? "#FFFFFF" : "#BDBDBD",
+            fontSize: 16,
+            fontFamily: "Roboto-Regular",
+          }}
+        >
+          Опубликовать
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setImage(null)} style={styles.delete}>
+        <Feather name="trash-2" size={24} color="#BDBDBD" />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -59,17 +113,17 @@ const styles = StyleSheet.create({
   fotoAction: {
     fontSize: 16,
     fontFamily: "Roboto-Regular",
-    alignItems: "flex-start"
-
+    alignItems: "flex-start",
   },
 
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "flex-start",
+    justifyContent: "space-between",
     paddingTop: 32,
     paddingLeft: 16,
     paddingRight: 16,
+    backgroundColor: "#FFFFFF",
   },
 
   addfoto: {
@@ -102,6 +156,16 @@ const styles = StyleSheet.create({
     magrinTop: 32,
     position: "absolute",
   },
+
+  fotoAction: {
+    width: "100%",
+    fontSize: 16,
+    fontFamily: "Roboto-Regular",
+    color: "#BDBDBD",
+    alignItems: "flex-start",
+    marginBottom: 32,
+  },
+
   name: {
     fontSize: 16,
     fontFamily: "Roboto-Regular",
@@ -112,7 +176,9 @@ const styles = StyleSheet.create({
     borderColor: "#E8E8E8",
     marginBottom: 16,
   },
+
   location: {
+    flex: 1,
     fontSize: 16,
     fontFamily: "Roboto-Regular",
     alignItems: "flex-start",
@@ -120,6 +186,37 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     width: "100%",
     borderColor: "#E8E8E8",
-    marginBottom: 16,
+    paddingLeft: 24,
+    marginLeft: -24,
+  },
+
+  locationSection: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  locationIcon: {
+    padding: 0,
+    color: "#BDBDBD",
+  },
+
+  createButton: {
+width: "100%",
+    fontSize: 16,
+    fontFamily: "Roboto-Regular",
+    paddingVertical: 16,
+    marginHorizontal: 16,
+    borderRadius: 50,
+    alignItems: "center",
+  },
+  delete: {
+    width: 70,
+    height: 40,
+    backgroundColor: "#F6F6F6",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    magrinTop: 120,
   },
 });

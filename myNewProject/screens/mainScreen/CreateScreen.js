b6b7769f5d509864fb2206cac67998dd3a2980cc
grynka <ihor.kozhemyakin@gmail.com>
2023-01-14 01:35:05 +0200

    upd

--- myNewProject/screens/mainScreen/CreateScreen.js
+++ myNewProject/screens/mainScreen/CreateScreen.js
@@ -12,116 +12,135 @@ import { FontAwesome } from "@expo/vector-icons";
 import { Feather, EvilIcons } from "@expo/vector-icons";
 import * as Location from "expo-location";
 
-export default function CreateScreen() {
+export default function CreateScreen({ navigation }) {
   const [image, setImage] = useState(null);
-  const [name, setName] = useState('');
-  const [location, setLocation] = useState('');
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
   const [geocode, setGeocode] = useState();
 
+  const startUserLocationUpdates = async () => {
+    if (Platform.OS !== "web") {
+      const { status } = await Location.requestForegroundPermissionsAsync();
 
- const startUserLocationUpdates = async () => { 
-if (Platform.OS !== "web") {
-  const { status } = await Location.requestForegroundPermissionsAsync();
-  
-  if (status !== "granted") {
-    Alert.alert(
-      "Insufficient permissions!",
-      "Sorry, we need location permissions to make this work!",
-      [{ text: "Okay" }]
-    );
-    return;
-  }
-}
-  }
-const  getGeocodeAsync= async () => {
-    let geocode = await Location.reverseGeocodeAsync(location)
-    setGeocode({geocode})
-    setGeocode(({geocode}) => geocode[0])
+      if (status !== "granted") {
+        Alert.alert(
+          "Insufficient permissions!",
+          "Sorry, we need location permissions to make this work!",
+          [{ text: "Okay" }]
+        );
+        return;
+      }
+    }
+  };
+  const getGeocodeAsync = async () => {
+    let geocode = await Location.reverseGeocodeAsync(location);
+    setGeocode({ geocode });
+    setGeocode(({ geocode }) => geocode[0]);
     //console.log(geocode[0].region, geocode[0].country)
-    console.log(geocode)
-    console.log(location)
-    return geocode
-  }
-  
+    console.log(geocode);
+    console.log(location);
+    return geocode;
+  };
 
-const nameHandler = (text) => setName(text);
+  const nameHandler = (text) => setName(text);
 
   const pickImage = async () => {
-    startUserLocationUpdates()
+    startUserLocationUpdates();
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     });
-    const loc = await Location.getCurrentPositionAsync()
+    const loc = await Location.getCurrentPositionAsync();
 
     console.log(result);
 
     if (!result.canceled) {
-      setLocation({ latitude: loc.coords.latitude, longitude: loc.coords.longitude})
+      setLocation({
+        latitude: loc.coords.latitude,
+        longitude: loc.coords.longitude,
+      });
       setImage(result.assets[0].uri);
-    }   
-    getGeocodeAsync()
+    }
+    getGeocodeAsync();
   };
+
+ const sendFoto = () => {
+  console.log(navigation)
+  navigator.navigate("PostsScreen", {Image});
+ };
 
   return (
     <View style={styles.container}>
-    <View style={styles.head}>
-      <View style={styles.addfoto}>
-        {image && <Image source={{ uri: image }} style={styles.foto} />}
-        <TouchableOpacity
-          title="Pick an image from camera roll"
-          onPress={pickImage}
-          style={{
-            ...styles.addfotoIcon,
-            backgroundColor: image ? "#FFFFFF4D" : "#FFFFFF",
-          }}
-        >
-          <FontAwesome
-            name="camera"
-            size={20}
-            color={image ? "#FFFFFF" : "#BDBDBD"}
-          />
-        </TouchableOpacity>
-      </View>
-      <Text style={styles.fotoAction}>
-        {image ? "Редактировать фото" : "Загрузите фото"}
-      </Text>
-      <TextInput placeholder="Название..." style={styles.name} value={name}
-                  onChangeText={nameHandler}/>
-      <View style={styles.locationSection}>
-      <TouchableOpacity onPress={getGeocodeAsync}>
-        <EvilIcons name="location" style={styles.locationIcon} size={24}  /></TouchableOpacity>
-        <TextInput placeholder="Местность..." style={styles.location} value={geocode ? `${geocode.region}, ${geocode.country}`  : ''} />
-        
-      </View>
-      <TouchableOpacity
-        activeOpacity={0.8}
-        style={{
-          ...styles.createButton,
-          backgroundColor: image ? "#FF6C00" : "#F6F6F6",
-        }}
-        onPress={getGeocodeAsync}
-      >
-        <Text
-          style={{
-            color: image ? "#FFFFFF" : "#BDBDBD",
-            fontSize: 16,
-            fontFamily: "Roboto-Regular",
-          }}
-        >
-          Опубликовать
+      <View style={styles.head}>
+        <View style={styles.addfoto}>
+          {image && <Image source={{ uri: image }} style={styles.foto} />}
+          <TouchableOpacity
+            title="Pick an image from camera roll"
+            onPress={pickImage}
+            style={{
+              ...styles.addfotoIcon,
+              backgroundColor: image ? "#FFFFFF4D" : "#FFFFFF",
+            }}
+          >
+            <FontAwesome
+              name="camera"
+              size={20}
+              color={image ? "#FFFFFF" : "#BDBDBD"}
+            />
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.fotoAction}>
+          {image ? "Редактировать фото" : "Загрузите фото"}
         </Text>
-      </TouchableOpacity>
+        <TextInput
+          placeholder="Название..."
+          style={styles.name}
+          value={name}
+          onChangeText={nameHandler}
+        />
+        <View style={styles.locationSection}>
+          <TouchableOpacity onPress={getGeocodeAsync}>
+            <EvilIcons name="location" style={styles.locationIcon} size={24} />
+          </TouchableOpacity>
+          <TextInput
+            onFocus={getGeocodeAsync}
+            placeholder="Местность..."
+            style={styles.location}
+            value={geocode ? `${geocode.region}, ${geocode.country}` : ""}
+          />
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={{
+            ...styles.createButton,
+            backgroundColor: image ? "#FF6C00" : "#F6F6F6",
+          }}
+          onPress={sendFoto}
+        >
+          <Text
+            style={{
+              color: image ? "#FFFFFF" : "#BDBDBD",
+              fontSize: 16,
+              fontFamily: "Roboto-Regular",
+            }}
+          >
+            Опубликовать
+          </Text>
+        </TouchableOpacity>
       </View>
 
       <View style={styles.bottom}>
-      <TouchableOpacity onPress={() => {setImage(null), setGeocode(null)}} style={styles.delete}>
-        <Feather name="trash-2" size={24} color="#BDBDBD" />
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setImage(null), setGeocode(null);
+          }}
+          style={styles.delete}
+        >
+          <Feather name="trash-2" size={24} color="#BDBDBD" />
+        </TouchableOpacity>
       </View>
-
     </View>
   );
 }
@@ -148,7 +167,7 @@ const styles = StyleSheet.create({
     paddingBottom: 32,
     backgroundColor: "#FFFFFF",
     borderTopWidth: 1,
-    borderColor: "#212121"
+    borderColor: "#212121",
   },
 
   addfoto: {
@@ -227,7 +246,7 @@ const styles = StyleSheet.create({
   },
 
   createButton: {
-width: "100%",
+    width: "100%",
     fontSize: 16,
     fontFamily: "Roboto-Regular",
     paddingVertical: 16,
--- myNewProject/screens/mainScreen/PostsScreen.js
+++ myNewProject/screens/mainScreen/PostsScreen.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import DefaultPostsScreen from "../NestedScreen/DefaultScreenPosts";
 import CommentsScreen from "../NestedScreen/CommentsScreens";
+import { View, FlatList, Image } from "react-native";
 
 const NestedScreens = createStackNavigator();
 
-const PostsScreen = () => {
-  return(
-<NestedScreens.Navigator>
-  <NestedScreens.Screen name="Публикации" component={ DefaultPostsScreen } />
-  <NestedScreens.Screen name="Коментарии" component={ DefaultPostsScreen } />
-</NestedScreens.Navigator>)
-}
+const PostsScreen = ({ route }) => {
+  const [posts, setPosts] = useState([]);
 
-export default PostsScreen
+  useEffect(() => {
+   if (route.params) {
+    setPosts((prevState) => [...prevState, route.params]);
+   }
+    
+  }, [route.params]);
+
+  return (
+    <>
+      <FlatList
+        data={posts}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Image source={{ uri: item.photo }} />}
+      />
+
+      <NestedScreens.Navigator>
+        <NestedScreens.Screen
+          name="Публикации"
+          component={DefaultPostsScreen}
+        />
+        <NestedScreens.Screen name="Коментарии" component={CommentsScreen} />
+      </NestedScreens.Navigator>
+    </>
+  );
+};
+
+export default PostsScreen;
